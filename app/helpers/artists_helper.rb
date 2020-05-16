5dadd2047c8b4826460a3cb50b1e3fab94fb4e0c
Sheena Bonilla <sheena.myb@gmail.com>
2020-05-16 22:58:14 +0000

    Automatically backed up by Learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,6 +1,6 @@
 module ArtistsHelper
   def display_artist(artist)
-    if !artist.nil?
+    if artist
       link_to "<%= artist.name %>", artist_path(artist)
     else #not associated with a song
       link_to "Add Artist", edit_song_path(artist)
